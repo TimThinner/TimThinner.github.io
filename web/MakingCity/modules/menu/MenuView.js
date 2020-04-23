d0ba4ee67c384723a628e09bda7fc9a80dc7f0ee
Timo Kinnunen <timo.kinnunen@vtt.fi>
2020-04-23 16:30:54 +0300

    Added first step of animated line drawing to menupage.

--- web/MakingCity/modules/menu/MenuView.js
+++ web/MakingCity/modules/menu/MenuView.js
@@ -70,6 +70,21 @@ export default class MenuView extends View {
 			}, false);
 			hexA.addEventListener("mouseover", function(event){ self.setHoverEffect(event, 'scale(1.1)'); }, false);
 			hexA.addEventListener("mouseout", function(event){ self.setHoverEffect(event, 'scale(1.0)'); }, false);
+			
+/*
+Animated line drawing in SVG:
+http://bkaprt.com/psvg/07-17/
+http://bkaprt.com/psvg/07-18/
+NOTE: Values stroke-dasharray set to 0px 0px and animate from to 0 in SVG-file.
+These are filled with correct values in here:
+*/
+			const path = svgObject.getElementById('first-building-path');
+			var len = path.getTotalLength();
+			console.log(['len=',len]);
+			//stroke-dasharray:700px 700px
+			path.style.strokeDasharray = len+'px '+len+'px';
+			const anim = svgObject.getElementById('first-building-path-animate');
+			anim.setAttributeNS(null, 'from', len);
 		}
 	}
 	

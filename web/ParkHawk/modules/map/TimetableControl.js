37d49d8ec6eca445bac32570c193c2fc2bbe2734
Timo Kinnunen <timo.kinnunen@vtt.fi>
2020-06-12 12:26:10 +0300

    Added more functionality to bus stop timetables.

--- web/ParkHawk/modules/map/TimetableControl.js
+++ web/ParkHawk/modules/map/TimetableControl.js
@@ -6,12 +6,16 @@ export default class TimetableControl {
 		this.mapview = mapview;
 		this.mymap = mapview.mymap;
 		
+		this.offset = 0;
+		this.MAXLINES = 8;
 		this.bustimetables = undefined;
 		this.timetableIndexPage = undefined;
 		this.boundBusScheduleHandler = (ev) => this.busScheduleHandler(ev);
 		this.boundDivHandler = (ev) => this.divHandler(ev);
 		this.boundLinkHandler = (ev, context) => this.linkHandler(ev, context);
 		this.boundCloseHandler = (ev) => this.closeHandler(ev);
+		this.boundUpHandler = (ev) => this.upHandler(ev);
+		this.boundDownHandler = (ev) => this.downHandler(ev);
 	}
 	
 	hideBusTimetables() {
@@ -29,8 +33,19 @@ export default class TimetableControl {
 	
 	linkHandler(ev) {
 		L.DomEvent.stopPropagation(ev);
-		console.log(['link clicked ev.target=',ev.target]);
+		console.log(['link clicked ev.target.id=',ev.target.id]);
 		//console.log(['link clicked target=',target]);
+		// stopname-0 ....  stopname-7
+		
+		const ind = parseInt(ev.target.id.slice(9), 10);
+		let stopnames = [];
+		const MM = this.mapview.getModel('MapModel');
+		if (typeof MM !== 'undefined') {
+			if (MM.BusStopData && MM.BusStopData.stopnames) {
+				stopnames = MM.BusStopData.stopnames;
+			}
+		}
+		console.log(['Selected Stop = ',stopnames[this.offset+ind]]);
 	}
 	
 	closeHandler(ev) {
@@ -38,9 +53,58 @@ export default class TimetableControl {
 		this.hideBusTimetables();
 	}
 	
+	upHandler(ev) {
+		L.DomEvent.stopPropagation(ev);
+		console.log('up handler');
+		let stopnames = [];
+		const MM = this.mapview.getModel('MapModel');
+		if (typeof MM !== 'undefined') {
+			if (MM.BusStopData && MM.BusStopData.stopnames) {
+				stopnames = MM.BusStopData.stopnames;
+			}
+		}
+		const stopcount = stopnames.length;
+		if (this.offset > 0) {
+			this.offset--;
+		}
+		let maxrows = this.MAXLINES;
+		if (stopcount < maxrows) {
+			maxrows = stopcount;
+		}
+		for(let i = 0; i < maxrows; i++) {
+			$('#stopname-'+i).empty().append(stopnames[i+this.offset]);
+		}
+	}
+	
+	downHandler(ev) {
+		L.DomEvent.stopPropagation(ev);
+		console.log('down handler');
+		let stopnames = [];
+		const MM = this.mapview.getModel('MapModel');
+		if (typeof MM !== 'undefined') {
+			if (MM.BusStopData && MM.BusStopData.stopnames) {
+				stopnames = MM.BusStopData.stopnames;
+			}
+		}
+		const stopcount = stopnames.length;
+		const limit = stopcount-this.MAXLINES;
+		if (this.offset < limit) {
+			this.offset++;
+		}
+		
+		let maxrows = this.MAXLINES;
+		if (stopcount < maxrows) {
+			maxrows = stopcount;
+		}
+		for(let i = 0; i < maxrows; i++) {
+			$('#stopname-'+i).empty().append(stopnames[i+this.offset]);
+		}
+	}
+	
 	busScheduleHandler(ev) {
 		L.DomEvent.stopPropagation(ev);
 		const self = this;
+		
 		if (typeof this.timetableIndexPage === 'undefined') {
 			
 			let stopnames = [];
@@ -50,11 +114,12 @@ export default class TimetableControl {
 					stopnames = MM.BusStopData.stopnames;
 				}
 			}
+			console.log(['stopnames=',stopnames]);
+			const offs = this.offset;
 			const stopcount = stopnames.length;
-			const maxrows = 8;
-			let rowoffset = 0;
-			if (stopcount > maxrows) {
-				
+			let maxrows = this.MAXLINES;
+			if (stopcount < maxrows) {
+				maxrows = stopcount;
 			}
 			
 			this.timetableIndexPage = L.control({position: 'topright'});
@@ -66,26 +131,39 @@ export default class TimetableControl {
 				this._div_LCol = L.DomUtil.create('div', 'col s10', this._div_Row);
 				
 				this._div_Stops = [];
-				for (let i = rowoffset; i < maxrows; i++) {
+				for (let i = 0; i < maxrows; i++) {
 					
 					this._div_Stops[i] = L.DomUtil.create('div', 'timetable-index-page-div', this._div_LCol);
-					this._div_Stops[i].innerHTML = '<a id="stopname-'+i+'">'+stopnames[i]+'</a>';
+					this._div_Stops[i].innerHTML = '<a id="stopname-'+i+'">'+stopnames[i+offs]+'</a>';
 					L.DomEvent.on(this._div_Stops[i], 'click dblclick', self.boundLinkHandler);
 				}
 				
 				this._div_RCol = L.DomUtil.create('div', 'col s2', this._div_Row);
-				this._img = L.DomUtil.create('img', 'timetable-button', this._div_RCol);
-				this._img.src = 'assets/x.svg';
-				L.DomEvent.on(this._img, 'click dblclick', self.boundCloseHandler);
+				this._img_close = L.DomUtil.create('img', 'timetable-button', this._div_RCol);
+				this._img_close.src = 'assets/x.svg';
+				L.DomEvent.on(this._img_close, 'click dblclick', self.boundCloseHandler);
+				
+				if (stopcount > 8) {
+					this._img_up = L.DomUtil.create('img', 'timetable-button', this._div_RCol);
+					this._img_up.src = 'assets/arrowup.svg';
+					this._img_down = L.DomUtil.create('img', 'timetable-button', this._div_RCol);
+					this._img_down.src = 'assets/arrowdown.svg';
+					L.DomEvent.on(this._img_up, 'click dblclick', self.boundUpHandler);
+					L.DomEvent.on(this._img_down, 'click dblclick', self.boundDownHandler);
+				}
 				L.DomEvent.on(this._div, 'click dblclick', self.boundDivHandler);
 				return this._div;
 			};
 			this.timetableIndexPage.onRemove = function (map) {
 				console.log('timetableIndexPage now Removed!');
-				for (let i = rowoffset; i < maxrows; i++) {
+				for (let i = 0; i < maxrows; i++) {
 					L.DomEvent.off(this._div_Stops[i], 'click dblclick', self.boundLinkHandler);
 				}
-				L.DomEvent.off(this._img, 'click dblclick', self.boundCloseHandler);
+				L.DomEvent.off(this._img_close, 'click dblclick', self.boundCloseHandler);
+				if (stopcount > 8) {
+					L.DomEvent.off(this._img_up, 'click dblclick', self.boundUpHandler);
+					L.DomEvent.off(this._img_down, 'click dblclick', self.boundDownHandler);
+				}
 				L.DomEvent.off(this._div, 'click dblclick', self.boundDivHandler);
 			};
 			this.timetableIndexPage.addTo(this.mymap);

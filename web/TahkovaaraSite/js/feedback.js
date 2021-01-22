1a4b56147c12ca671994fac4a3bd7cb5d6c68034
Timo Kinnunen <timo.kinnunen@vtt.fi>
2021-01-22 15:58:21 +0200

    Added images to gallery and PDFs to pdf directory.

---
+++ web/TahkovaaraSite/js/feedback.js
@@ -0,0 +1,23 @@
+
+/*
+Palautelomake
+
+Kirjoita arvostelu
+Arvosana
+Valikko 1-5 tähteä
+Arvostelu (julkaistaan sivustolla)
+•	Vapaa tekstikenttä
+Yksityinen viesti, joka näkyy vain mökin omistajalle
+•	Vapaa tekstikenttä
+
+Nimi tai nimimerkki
+Sähköpostiosoite
+
+Olen vierailut kohteessa
+•	Valikot: kuukausi ja vuosi
+Lähetä arvostelu
+Teksti: Kiitos arvostelustasi, tarkistamme ja lisäämme sen sivustolle lähiaikoina.
+
+
+
+*/

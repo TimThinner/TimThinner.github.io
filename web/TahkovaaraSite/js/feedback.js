9cbaec269fca8696d8b9cf27dbe7b91aecf00332
Timo Kinnunen <timo.kinnunen@vtt.fi>
2021-01-26 11:50:13 +0200

    Added feedback from feedback.json file.

--- web/TahkovaaraSite/js/feedback.js
+++ web/TahkovaaraSite/js/feedback.js
@@ -63,8 +63,6 @@ Teksti: Kiitos arvostelustasi, tarkistamme ja lisäämme sen sivustolle lähiaik
 		$('#feedback-send-wrapper').empty();
 	}
 	
-	
-	
 	feedbackReportError = function(errors) {
 		const message = errors.join('<br/>');
 		const html = '<div class="error-message"><p>'+message+'</p></div>';
@@ -75,12 +73,13 @@ Teksti: Kiitos arvostelustasi, tarkistamme ja lisäämme sen sivustolle lähiaik
 		const messages = [];
 		
 		// Check that all params are included and there are no conflicting data in query.
-		
-		
+		if (typeof feedbackRating === 'undefined') {
+			messages.push('Arvostelu puuttuu!');
+		}
 		
 		feedbackText = $('#public-feedback').val();
 		if (feedbackText.length === 0) {
-			messages.push('Arvostelu puuttuu!');
+			messages.push('Viesti puuttuu!');
 		}
 		
 		feedbackName = $('#feedback-name').val();
@@ -113,8 +112,8 @@ Teksti: Kiitos arvostelustasi, tarkistamme ja lisäämme sen sivustolle lähiaik
 			$('#feedback-send-wrapper').empty().append(mailto);
 			
 		} else {
-			
-			const body = 'Arvostelu%3A%20'+feedbackText + private_message +
+			const body = 'Tähtiä%3A%20'+feedbackRating+
+			'%0D%0AViesti%3A%20'+feedbackText + private_message +
 			'%0D%0ANimi%3A%20'+feedbackName+
 			'%0D%0AKuukausi%3A%20'+feedbackVisitMonthName+
 			'%0D%0AVuosi%3A%20'+feedbackVisitYear+
@@ -145,11 +144,69 @@ Teksti: Kiitos arvostelustasi, tarkistamme ja lisäämme sen sivustolle lähiaik
 		const mailto = '<a class="waves-effect waves-light btn disabled" href="mailto:tahkovaara@intelcon.fi?subject=Arvostelu">LÄHETÄ<i class="material-icons right">send</i></a>';
 		$('#feedback-send-wrapper').empty().append(mailto);
 		
+		const rating_markup = 
+			'<div class="row">'+
+				'<div class="col s12 center">'+
+					'<a href="javascript:void(0);" id="r-1" style="color:#ccc;"><i style="font-size:3rem;" class="material-icons">star</i></a>'+
+					'<a href="javascript:void(0);" id="r-2" style="color:#ccc;"><i style="font-size:3rem;" class="material-icons">star</i></a>'+
+					'<a href="javascript:void(0);" id="r-3" style="color:#ccc;"><i style="font-size:3rem;" class="material-icons">star</i></a>'+
+					'<a href="javascript:void(0);" id="r-4" style="color:#ccc;"><i style="font-size:3rem;" class="material-icons">star</i></a>'+
+					'<a href="javascript:void(0);" id="r-5" style="color:#ccc;"><i style="font-size:3rem;" class="material-icons">star</i></a>'+
+				'</div>'+
+			'</div>';
+		$('#feedback-rating-wrapper').empty().append(rating_markup);
+		
+		$("#r-1").on('click',function() {
+			feedbackRating = 1;
+			$("#r-1").attr('style', 'color:#fa0');
+			$("#r-2").attr('style', 'color:#ccc');
+			$("#r-3").attr('style', 'color:#ccc');
+			$("#r-4").attr('style', 'color:#ccc');
+			$("#r-5").attr('style', 'color:#ccc');
+			feedbackGenerateMailToLink();
+		});
+		$("#r-2").on('click',function() {
+			feedbackRating = 2;
+			$("#r-1").attr('style', 'color:#fa0');
+			$("#r-2").attr('style', 'color:#fa0');
+			$("#r-3").attr('style', 'color:#ccc');
+			$("#r-4").attr('style', 'color:#ccc');
+			$("#r-5").attr('style', 'color:#ccc');
+			feedbackGenerateMailToLink();
+		});
+		$("#r-3").on('click',function() {
+			feedbackRating = 3;
+			$("#r-1").attr('style', 'color:#fa0');
+			$("#r-2").attr('style', 'color:#fa0');
+			$("#r-3").attr('style', 'color:#fa0');
+			$("#r-4").attr('style', 'color:#ccc');
+			$("#r-5").attr('style', 'color:#ccc');
+			feedbackGenerateMailToLink();
+		});
+		$("#r-4").on('click',function() {
+			feedbackRating = 4;
+			$("#r-1").attr('style', 'color:#fa0');
+			$("#r-2").attr('style', 'color:#fa0');
+			$("#r-3").attr('style', 'color:#fa0');
+			$("#r-4").attr('style', 'color:#fa0');
+			$("#r-5").attr('style', 'color:#ccc');
+			feedbackGenerateMailToLink();
+		});
+		$("#r-5").on('click',function() {
+			feedbackRating = 5;
+			$("#r-1").attr('style', 'color:#fa0');
+			$("#r-2").attr('style', 'color:#fa0');
+			$("#r-3").attr('style', 'color:#fa0');
+			$("#r-4").attr('style', 'color:#fa0');
+			$("#r-5").attr('style', 'color:#fa0');
+			feedbackGenerateMailToLink();
+		});
+		
 		const textarea_markup = 
 			'<div class="row">'+
 				'<div class="input-field col s12">'+
 					'<textarea id="public-feedback" class="materialize-textarea"></textarea>'+
-					'<label for="public-feedback">Arvostelu (julkaistaan sivustolla)</label>'+
+					'<label for="public-feedback">Viesti (julkaistaan sivustolla)</label>'+
 				'</div>'+
 			'</div>';
 		$('#feedback-text-wrapper').empty().append(textarea_markup);
@@ -163,6 +220,7 @@ Teksti: Kiitos arvostelustasi, tarkistamme ja lisäämme sen sivustolle lähiaik
 				'</div>'+
 			'</div>';
 		$('#feedback-private-text-wrapper').empty().append(private_textarea_markup);
+		
 		
 		const visit_mmyyyy_markup =
 			'<p style="color:#888;">Olen vieraillut kohteessa:</p>'+
@@ -217,7 +275,6 @@ Teksti: Kiitos arvostelustasi, tarkistamme ja lisäämme sen sivustolle lähiaik
 				'</div>'+
 			'</div>';
 		$('#feedback-name-wrapper').empty().append(name_markup);
-		
 		
 		$('#public-feedback').on('change', function(){
 			feedbackGenerateMailToLink();
@@ -288,5 +345,64 @@ Teksti: Kiitos arvostelustasi, tarkistamme ja lisäämme sen sivustolle lähiaik
 	$('#feedback-cancel').on('click',function() {
 		feedbackReset();
 	});
+	
+	
+	
+	feedbackReportFetchError = function(error) {
+		let markup = '<p style="text-align:center;color:a00;font-weight:bold;">';
+		if (typeof error.message !== 'undefined') {
+			markup += error.message;
+		} else {
+			markup += 'Error without message has occurred.';
+		}
+		markup += '</p>';
+		$('#feedback-feed').empty().append(markup);
+	}
+	/*
+		Read feedback from file "feedback.json" and fill in the MARKUP tagged by 
+			#feedback-feed
+	*/
+	fetch('./json/feedback.json')
+		.then(function(response) {
+			status = response.status;
+			return response.json();
+		})
+		.then(function(myJson) {
+			
+			if (Array.isArray(myJson)) {
+				// #history-mobile: put all paragraphs within one div
+				
+				myJson.forEach(a=>{
+						//"message":"Kohde vastasi kohteen kuvausta.",
+						//"name":"Tanja Kotkanoja",
+						//"rating":5,
+						//"time":"Joulukuu 2018"
+					let rating_text = '';
+					const star = '<i style="font-size:1rem;color:#fa0;" class="material-icons">star</i>';
+					if (a.rating === 1) {
+						rating_text = star;
+					} else if (a.rating === 2) {
+						rating_text = star + star;
+					} else if (a.rating === 3) {
+						rating_text = star + star + star;
+					} else if (a.rating === 4) {
+						rating_text = star + star + star + star;
+					} else if (a.rating === 5) {
+						rating_text = star + star + star + star + star;
+					}
+					const html = '<div style="padding:12px;background-color:#444;color:#fff;border:1px solid #ccc;"><p>'+a.message+'<br/>'+a.name+' '+rating_text+'<br/>'+a.time+'</p></div>';
+					$(html).appendTo('#feedback-feed');
+				});
+				
+			} else {
+				feedbackReportFetchError({message:'Error: feedback.json content is NOT an Array.'});
+			}
+		})
+		.catch(error => {
+			console.log (['error=',error]);
+			feedbackReportFetchError(error);
+		});
+	
+	
 	
 })(jQuery);

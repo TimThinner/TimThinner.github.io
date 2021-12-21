d7e4a80a2639feaa2fac777a94ccc5b332e1049e
Timo Kinnunen <timo.kinnunen@vtt.fi>
2021-12-21 11:48:19 +0200

    Testing.

--- web/HitchHiker/modules/menu/MenuView.js
+++ web/HitchHiker/modules/menu/MenuView.js
@@ -41,7 +41,8 @@ export default class MenuView extends View {
 		if (this.controller.visible) {
 			if (options.model==='ResizeEventObserver' && options.method==='resize') {
 				if (this.rendered===true) {
-					this.createBall();
+					//this.createBall();
+					this.createBackground();
 				} else {
 					this.render();
 				}
@@ -49,9 +50,112 @@ export default class MenuView extends View {
 		}
 	}
 	
+	/*
+	<defs>
+	<radialGradient id="grad" cx="50%" cy="50%" r="100%">
+		<stop offset="10%" style="stop-color:#fff; stop-opacity:1" />
+		<stop offset="50%" style="stop-color:#eee; stop-opacity:1" />
+		<stop offset="90%" style="stop-color:#ddd; stop-opacity:1" />
+	</radialGradient>
+	</defs>
+	<rect x="-900" y="-500" width="1800" height="900" fill="url(#grad)" stroke-width="0" stroke="#000" />
+	*/
+	createBackground() {
+		$(this.el).empty();
+		
+		const w = this.REO.width-18; // We don't want scroll bars to the right or bottom of view.
+		const h = this.REO.height-18;
+		const wp2 = w*0.5;
+		const hp2 = h*0.5;
+		const vb = '-'+wp2+' -'+hp2+' '+w+' '+h;
+		
+		const svgNS = 'http://www.w3.org/2000/svg';
+		const svg = document.createElementNS(svgNS, "svg");
+		svg.setAttributeNS(null,'width',w);
+		svg.setAttributeNS(null,'height',h);
+		svg.setAttributeNS(null,'viewBox',vb);
+		
+		// Store an array of stop information for the <linearGradient>
+		const stops = [
+			{
+				"style": "stop-color:#fff; stop-opacity:1",
+				"offset": "10%"
+			},
+			{
+				"style": "#stop-color:#eee; stop-opacity:1",
+				"offset": "50%"
+			},
+			{
+				"style": "#stop-color:#ddd; stop-opacity:1",
+				"offset": "90%"
+			}
+		];
+		const defs = document.createElementNS(svgNS, 'defs');
+		const gradient = document.createElementNS(svgNS, 'radialGradient');
+		const rect = document.createElementNS(svgNS, 'rect');
+		
+		// Parses an array of stop information and appends <stop> elements to the <linearGradient>
+		for (let i = 0, length = stops.length; i < length; i++) {
+			// Create a <stop> element and set its offset based on the position of the for loop.
+			const stop = document.createElementNS(svgNS, 'stop');
+			stop.setAttribute('offset', stops[i].offset);
+			stop.setAttribute('style', stops[i].style);
+			// Add the stop to the <lineargradient> element.
+			gradient.appendChild(stop);
+		}
+		// Apply the <lineargradient> to <defs>
+		gradient.id = 'grad';
+		gradient.setAttribute('cx', '50%');
+		gradient.setAttribute('cy', '50%');
+		gradient.setAttribute('r', '100%');
+		defs.appendChild(gradient);
+		
+		// Setup the <rect> element.
+		rect.setAttribute('x',-wp2);
+		rect.setAttribute('y',-hp2);
+		rect.setAttribute('width',w);
+		rect.setAttribute('height',h);
+		rect.setAttribute('fill', 'url(#grad)');
+		
+		//rect.setAttribute('width', '100%');
+		//rect.setAttribute('height', '100%');
+		
+		// Assign an id, classname, width and height
+		//svg.setAttribute('width', '100%');
+		//svg.setAttribute('height', '100%')
+		//svg.setAttribute('version', '1.1');
+		//svg.setAttribute('xmlns', svgNS);
+		
+		// Add the <defs> and <rect> elements to <svg>
+		svg.appendChild(defs);
+		svg.appendChild(rect);
+		
+		// Add the <svg> element to <body>
+		//document.body.appendChild(svg);
+		/*
+		const defs = document.createElementNS(svgNS,'defs');
+		svg.appendChild(defs);
+		
+		pathdef = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+		pathdef.id = "conn1";
+		pathdef.setAttributeNS(null, "d", "M264 133 L396 132");
+		defs.appendChild(pathdef);
+		
+		const rect = document.createElementNS(svgNS,'rect');
+		rect.setAttribute('x',-wp2);
+		rect.setAttribute('y',-hp2);
+		rect.setAttribute('width',w);
+		rect.setAttribute('height',h);
+		rect.setAttribute('fill','url(#grad)');
+		svg.appendChild(rect);
+		//document.body.appendChild(svg);
+		*/
+		$(this.el).append(svg);
+	}
+	
 	createBall() {
 		//$('#'+this.BALLID).empty();
-		$(this.el).empty();
+		//$(this.el).empty();
 		
 		const w = this.REO.width-18; // We don't want scroll bars to the right or bottom of view.
 		const h = this.REO.height-18;
@@ -115,7 +219,8 @@ export default class MenuView extends View {
 		$(html).appendTo(this.el);
 		*/
 		this.rendered = true;
-		this.createBall();
+		this.createBackground();
+		//this.createBall();
 		/*
 		if (this.areModelsReady()) {
 			const errorMessages = this.modelsErrorMessages();

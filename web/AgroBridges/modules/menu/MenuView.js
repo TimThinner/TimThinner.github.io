02fb7b1eb0613468b39f4438f7110b6cc2a090ad
Timo Kinnunen <timo.kinnunen@vtt.fi>
2022-03-29 16:41:14 +0300

    Added logo to front page.

--- web/AgroBridges/modules/menu/MenuView.js
+++ web/AgroBridges/modules/menu/MenuView.js
@@ -42,8 +42,8 @@ export default class MenuView extends View {
 		const html = '<div class="row">'+
 				'<div class="col s12">'+
 					'<div class="col s12 center">'+
-						'<h1>This is the Menu page for AgroBridges</h1>'+
-						'<p>&nbsp;</p>'+
+						'<h1>AgroBridges</h1>'+
+						'<p><img src="./img/logo.png" height="108"/></p>'+ // Original logo is 1920 x 1080 pixels.
 						'<button class="btn waves-effect waves-light" id="login">Login</button>'+
 					'</div>'+
 				'</div>'+

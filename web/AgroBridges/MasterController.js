919237317fae3545c8ce5e20d90cc1eca0f78d0f
Timo Kinnunen <timo.kinnunen@vtt.fi>
2022-03-29 14:12:25 +0300

    Added first draft of  farm view part 2.

--- web/AgroBridges/MasterController.js
+++ web/AgroBridges/MasterController.js
@@ -46,7 +46,7 @@ class MasterController {
 	
 	
 	init() {
-		console.log('MasterController init! Version 22.03.29-J');
+		console.log('MasterController init! Version 22.03.29-K');
 		
 		console.log('Create ResizeEventObserver!');
 		const REO = new ResizeEventObserver();

165dcdb3dd2919fc97cec7b6180d317be97955ac
Timo Kinnunen <timo.kinnunen@vtt.fi>
2022-04-26 15:21:09 +0300

    Added distance sliders to Farm Location.

--- web/AgroBridges/modules/location/LocationView.js
+++ web/AgroBridges/modules/location/LocationView.js
@@ -184,13 +184,19 @@ export default class LocationView extends View {
 						'<h6 class="required">In which country is your farm located?</h6>'+
 						'<div id="countries-wrapper"></div>'+
 					'</div>'+
-				'</div>'+
-			'</div>'+
-			'<div class="row">'+
-				'<div class="col s12">'+
 					'<div class="col s12 center">'+
 						'<h6 class="required">In which region is your farm located?</h6>'+
 						'<div id="regions-wrapper"></div>'+
+					'</div>'+
+					'<div class="input-field col s12">'+
+						'<h6>How long is the driving distance to the next bigger town?</h6>'+
+						'<p>&nbsp;</p>'+
+						'<div id="distance-bigger-town-slider"></div>'+
+					'</div>'+
+					'<div class="input-field col s12">'+
+						'<h6>How long is the driving distance to the major city?</h6>'+
+						'<p>&nbsp;</p>'+
+						'<div id="distance-major-city-slider"></div>'+
 					'</div>'+
 				'</div>'+
 			'</div>'+
@@ -210,92 +216,59 @@ export default class LocationView extends View {
 			this.resetCountrySelect();
 		}
 		
+		// Restore current selection:
+		const distance_to_town = this.USER_MODEL.profile.Distance_Drive_small;
+		const distance_to_city = this.USER_MODEL.profile.Distance_Drive_major;
+		
+		const distanceTownSlider = document.getElementById('distance-bigger-town-slider');
+		noUiSlider.create(distanceTownSlider, {
+			start: [distance_to_town],
+			connect: 'lower',
+			tooltips: [wNumb({decimals: 0})],
+			step: 1,
+			keyboardSupport: true,      // Default true
+			keyboardDefaultStep: 5,     // Default 10
+			keyboardPageMultiplier: 5,  // Default 5
+			keyboardMultiplier: 5,      // Default 1
+			range: {
+				'min': [0],
+				'max': [1000]
+			}
+		});
+		distanceTownSlider.noUiSlider.on('change', function (values) {
+			console.log(['values=',values]);
+			if (Array.isArray(values) && values.length > 0) {
+				self.USER_MODEL.profile.Distance_Drive_small = Math.round(values[0]);
+				// DATABASE Update USER_MODEL
+			}
+		});
+		
+		const distanceCitySlider = document.getElementById('distance-major-city-slider');
+		noUiSlider.create(distanceCitySlider, {
+			start: [distance_to_city],
+			connect: 'lower',
+			tooltips: [wNumb({decimals: 0})],
+			step: 1,
+			keyboardSupport: true,      // Default true
+			keyboardDefaultStep: 5,     // Default 10
+			keyboardPageMultiplier: 5,  // Default 5
+			keyboardMultiplier: 5,      // Default 1
+			range: {
+				'min': [0],
+				'max': [1000]
+			}
+		});
+		distanceCitySlider.noUiSlider.on('change', function (values) {
+			console.log(['values=',values]);
+			if (Array.isArray(values) && values.length > 0) {
+				self.USER_MODEL.profile.Distance_Drive_major = Math.round(values[0]);
+				// DATABASE Update USER_MODEL
+			}
+		});
+		
 		$("#location-ok").on('click', function() {
 			self.models['MenuModel'].setSelected('farm');
 		});
 		this.rendered = true;
 	}
 }
-/*
-						'<table class="striped">'+
-							'<thead>'+
-								'<tr>'+
-									'<th>Question</th>'+
-									'<th>Variables</th>'+
-								'</tr>'+
-							'</thead>'+
-							'<tbody>'+
-								'<tr>'+
-									'<td>In which country is your farm located?</td>'+
-									'<td>Country, NUTS3</td>'+
-								'</tr>'+
-								'<tr>'+
-									'<td>How long is the driving distance to the next bigger town?</td>'+
-									'<td>Distance_Drive_small</td>'+
-								'</tr>'+
-								'<tr>'+
-									'<td>How long is the driving distance to the major city?</td>'+
-									'<td>Distance_Drive_major</td>'+
-								'</tr>'+
-							'</tbody>'+
-						'</table>'+
-*/
-
-/*
-						'<select class="select-country" style="width:75%">'+ // id="id-select-country">'+
-							'<option value="BE">Belgique/België</option>'+
-							'<option value="BG">България</option>'+
-							'<option value="CZ">Česko</option>'+
-							'<option value="DK">Danmark</option>'+
-							'<option value="DE">Deutschland</option>'+
-							'<option value="EE">Eesti</option>'+
-							'<option value="IE">Éire/Ireland</option>'+
-							'<option value="EL">Ελλάδα</option>'+
-							'<option value="ES">España</option>'+
-							'<option value="FR">France</option>'+
-							'<option value="HR">Hrvatska</option>'+
-							'<option value="IT">Italia</option>'+
-							'<option value="CY">Κύπρος</option>'+
-							'<option value="LV">Latvija</option>'+
-							'<option value="LT">Lietuva</option>'+
-							'<option value="LU">Luxembourg</option>'+
-							'<option value="HU">Magyarország</option>'+
-							'<option value="MT">Malta</option>'+
-							'<option value="NL">Nederland</option>'+
-							'<option value="AT">Österreich</option>'+
-							'<option value="PL">Polska</option>'+
-							'<option value="PT">Portugal</option>'+
-							'<option value="RO">România</option>'+
-							'<option value="SI">Slovenija</option>'+
-							'<option value="SK">Slovensko</option>'+
-							'<option value="FI">Suomi/Finland</option>'+
-							'<option value="SE">Sverige</option>'+
-							'<option value="UK">United Kingdom</option>'+
-							'<option value="IS">Ísland</option>'+
-							'<option value="LI">Liechtenstein</option>'+
-							'<option value="NO">Norge</option>'+
-							'<option value="CH">Schweiz/Suisse/Svizzera</option>'+
-							'<option value="ME">Црна Гора</option>'+
-							'<option value="MK">Северна Македонија</option>'+
-							'<option value="AL">Shqipëria</option>'+
-							'<option value="RS">Srbija/Сpбија</option>'+
-							'<option value="TR">Türkiye</option>'+
-						'</select>'+
-*/
-
-		/*
-		// See: https://select2.org/programmatic-control/events
-		$('.select-country').on("select2:open", function (e) { 
-			console.log(["select2:open e=", e]);
-		});
-		$('.select-country').on("select2:close", function (e) { 
-			console.log(["select2:close e=", e]);
-		});
-		*/
-		/*$('.select-country').on("select2:unselect", function (e) { 
-			console.log(["select2:unselect e=", e]);
-		});
-		$('.select-country').on("change", function (e) { 
-			console.log(["select2:change e=", e]);
-		});
-		*/

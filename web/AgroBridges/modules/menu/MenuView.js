6c42799a2d235d4856f4b31ae7d0bc97cc4137be
Timo Kinnunen <timo.kinnunen@vtt.fi>
2022-04-27 16:42:47 +0300

    Updated version number.

--- web/AgroBridges/modules/menu/MenuView.js
+++ web/AgroBridges/modules/menu/MenuView.js
@@ -120,7 +120,7 @@ export default class MenuView extends View {
 					'</div>'+
 					'<div class="col s12 center">'+
 						//'<p style="color:#ccc;">W='+w+'px H='+h+'px</p>'+
-						'<p style="color:#ccc;">Version 22.04.27-D</p>'+
+						'<p style="color:#ccc;">Version 22.04.27-E</p>'+
 					'</div>'+
 				'</div>'+
 			'</div>';

2447e41e9e8a7f3d91124fe297c949b0c46ac2c2
Timo Kinnunen <timo.kinnunen@vtt.fi>
2022-04-29 16:01:21 +0300

    Added more spiders to analysis view part 9.

--- web/AgroBridges/modules/menu/MenuView.js
+++ web/AgroBridges/modules/menu/MenuView.js
@@ -120,7 +120,7 @@ export default class MenuView extends View {
 					'</div>'+
 					'<div class="col s12 center">'+
 						//'<p style="color:#ccc;">W='+w+'px H='+h+'px</p>'+
-						'<p style="color:#ccc;">Version 22.04.29-M</p>'+
+						'<p style="color:#ccc;">Version 22.04.29-N</p>'+
 					'</div>'+
 				'</div>'+
 			'</div>';

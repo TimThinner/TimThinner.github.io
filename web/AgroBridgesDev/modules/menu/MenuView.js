f74addb51a873d4340ea5c408cef4ae96d5a4523
Timo Kinnunen <timo.kinnunen@vtt.fi>
2022-06-22 14:35:44 +0300

    Added analysis simulation compatible with database result part 2.

--- web/AgroBridgesDev/modules/menu/MenuView.js
+++ web/AgroBridgesDev/modules/menu/MenuView.js
@@ -120,7 +120,7 @@ export default class MenuView extends View {
 					'</div>'+
 					'<div class="col s12 center">'+
 						//'<p style="color:#ccc;">W='+w+'px H='+h+'px</p>'+
-						'<p style="color:#ccc;">Version 22.06.22-Alfa</p>'+
+						'<p style="color:#ccc;">Version 22.06.22-Bravo</p>'+
 					'</div>'+
 				'</div>'+
 			'</div>';
